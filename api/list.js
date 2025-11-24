eb6b598dc1a742a6fddd594d4a09682a4f945f5c
MonEvade <168145856+Monmon9ne@users.noreply.github.com>
2025-11-24 15:58:54 +0700

    Create list.js

---
+++ api/list.js
@@ -0,0 +1,14 @@
+import { getStorage } from './storage.js';
+
+export default function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin','*');
+  res.setHeader('Access-Control-Allow-Methods','GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers','Content-Type');
+
+  if(req.method==='OPTIONS'){ return res.status(200).end(); }
+
+  if(req.method!=='GET'){ return res.status(405).json({error:'Method not allowed'}); }
+
+  const storage = getStorage();
+  res.status(200).json(storage);
+}
